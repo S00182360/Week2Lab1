63822c9b3e6311f53da0efa9cbaa78708ed8532d
Samuel Colville <S00182360@mail.itsligo.ie>
2018-09-19 13:06:09 +0100

    First commit for Mono Game

--- Week2Lab1/Game1.cs
+++ Week2Lab1/Game1.cs
@@ -1,16 +1,45 @@
-﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+//using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace Week2Lab1
 {
     /// <summary>
-    /// This is the main type for your game.
+    /// This is the main type for your game
     /// </summary>
-    public class Game1 : Game
+    public class Game1 : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        // Variables for the Dot
+        Texture2D dot;
+        Color dotColor;
+        Rectangle dotRect;
+        int dotSize;
+        // Variables for the Background 
+        Texture2D background;
+        Rectangle backgroundRect;
+
+        // Variables to hold the display properties
+        int displayWidth;
+        int displayHeight;
+
+        // Variables to hold the color change
+        byte redComponent = 0;
+        byte blueComponent = 0;
+        byte greenComponent = 0;
+        byte alphaComponent = 0;
+
+        // Vars to draw message
+        SpriteFont font;
+        string message = "";
 
         public Game1()
         {
@@ -27,6 +56,8 @@ namespace Week2Lab1
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            displayWidth = GraphicsDevice.Viewport.Width;
+            displayHeight = GraphicsDevice.Viewport.Height;
 
             base.Initialize();
         }
@@ -41,11 +72,21 @@ namespace Week2Lab1
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            dot = Content.Load<Texture2D>("WhiteDot");
+            dotColor = Color.White;
+            dotSize = 40;
+
+            dotRect = new Rectangle(graphics.GraphicsDevice.Viewport.Width / 2, graphics.GraphicsDevice.Viewport.Height / 2, dotSize, dotSize);
+
+            background = Content.Load<Texture2D>("background");
+            backgroundRect = new Rectangle(0, 0, displayWidth, displayHeight);
+            font = Content.Load<SpriteFont>("MessageFont");
+
         }
 
         /// <summary>
         /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
+        /// all content.
         /// </summary>
         protected override void UnloadContent()
         {
@@ -59,10 +100,51 @@ namespace Week2Lab1
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+            if (GamePad.GetState(PlayerIndex.One).Buttons.X == ButtonState.Pressed ||
+                            Keyboard.GetState().IsKeyDown(Keys.B))
+                blueComponent++;
+            if (GamePad.GetState(PlayerIndex.One).Buttons.A == ButtonState.Pressed ||
+                            Keyboard.GetState().IsKeyDown(Keys.G))
+                greenComponent++;
+            if (GamePad.GetState(PlayerIndex.One).Buttons.B == ButtonState.Pressed ||
+                            Keyboard.GetState().IsKeyDown(Keys.R))
+                redComponent++;
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Y == ButtonState.Pressed ||
+                            Keyboard.GetState().IsKeyDown(Keys.T))
+                alphaComponent++;
 
+            
+            
+            GamePadState gpState = GamePad.GetState(PlayerIndex.One);
+            if (gpState.IsConnected)
+            {
+                if (gpState.ThumbSticks.Left.X != 0 ||
+                    gpState.ThumbSticks.Left.Y != 0)
+                {
+                    if(dotRect.X + (int)(gpState.ThumbSticks.Left.X * 5) //*5 for scale pu factor and speed of movement.
+                        < graphics.GraphicsDevice.Viewport.Width - dotRect.Width &&
+                        dotRect.X + (int)(gpState.ThumbSticks.Left.X * 5) > 0)
+                    {
+                        dotRect.X += (int)(gpState.ThumbSticks.Left.X * 5);
+                    }
+                                
+                    
+                    if (dotRect.Y - (int)(gpState.ThumbSticks.Left.Y * 5) 
+                        < graphics.GraphicsDevice.Viewport.Height - dotRect.Height &&
+                        dotRect.Y - (int)(gpState.ThumbSticks.Left.Y * 5) > 0)
+                    
+                                dotRect.Y -= (int)(gpState.ThumbSticks.Left.Y * 5);
+                }
+            }
             // TODO: Add your update logic here
+            dotColor = new Color(redComponent, greenComponent, blueComponent, alphaComponent);
+            message = "Red: " + redComponent.ToString() +
+                            " Green: " + greenComponent.ToString() +
+                            " Blue: " + blueComponent.ToString() +
+                            " Alpha: " + alphaComponent.ToString();
 
             base.Update(gameTime);
         }
@@ -76,6 +158,18 @@ namespace Week2Lab1
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(background, backgroundRect, Color.White);
+
+            spriteBatch.Draw(dot, dotRect, dotColor);
+
+            // Work out Centre the text to draw
+            int stringWidth = (int)font.MeasureString(message).X;
+
+            spriteBatch.DrawString(font, message, new Vector2((displayWidth - stringWidth) / 2, 0), Color.White);
+
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
